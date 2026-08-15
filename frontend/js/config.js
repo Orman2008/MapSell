a5292bbdb16d679870c2c90b001ceafd4c900703
Orman2 <145046128+Orman2@users.noreply.github.com>
2026-08-16 03:10:11 +0500

    Update config

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,5 +1,5 @@
 // Replace this with your Railway public API URL.
 // Example: https://your-api.up.railway.app
 window.APP_CONFIG = {
-  API_BASE_URL: "mapsell-production.up.railway.app"
+  API_BASE_URL: "mapsell-production-f80f.up.railway.app"
 };
